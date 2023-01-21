561ee51fb1d4ab6326e01bb525dedbaa25e21d9b
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-01-21 14:46:19 -0500

    Started implementing front end design

--- Frontend/chefs-mate-frontend/src/App.js
+++ Frontend/chefs-mate-frontend/src/App.js
@@ -1,31 +1,89 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
   let userName = "Vaaranan"; // Hardcoded
   return (
-    <div className="App">
-      <header>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        backgroundColor: "#FFDE59",
+        height: "100vh",
+      }}
+    >
+      <div style={{ padding: "7.5%" }}>
+        <header
+          style={{
+            fontSize: "200%",
+            padding: "2.5%",
+            fontFamily: "Barlow Condensed",
+            fontWeight: "800",
+          }}
         >
-          Learn React
-        </a> */}
-        Welcome {userName}
-      </header>
-      <div>Please enter your dietary restrictions before moving forward:</div>
-      <input></input>
-      <div>What are you craving?:</div>
-      <input></input>
-      <div>What is your budget?:</div>
-      <input></input>
+          Hi {userName}!
+        </header>
+        <div>
+          <div
+            style={{
+              fontFamily: "Barlow Condensed",
+              fontWeight: "400",
+              marginBottom: "0.5%",
+              fontSize: 20,
+            }}
+          >
+            Please enter your dietary restrictions before moving forward:
+          </div>
+
+          <input style={{ marginBottom: "1.5%" }}></input>
+        </div>
+        <div>
+          <div
+            style={{
+              fontFamily: "Barlow Condensed",
+              fontWeight: "400",
+              marginBottom: "0.5%",
+              fontSize: 20,
+            }}
+          >
+            What are you craving?:
+          </div>
+          <div
+            style={{
+              marginBottom: "1.5%",
+            }}
+          >
+            <input></input>
+          </div>
+        </div>
+
+        <div>
+          <div
+            style={{
+              fontFamily: "Barlow Condensed",
+              fontWeight: "400",
+              marginBottom: "0.5%",
+              fontSize: 20,
+            }}
+          >
+            What is your budget?:
+          </div>
+          <input style={{ marginBottom: "1.5%" }}></input>
+        </div>
+        <div>
+          <div
+            style={{
+              fontFamily: "Barlow Condensed",
+              fontWeight: "400",
+              marginBottom: "0.5%",
+              fontSize: 20,
+            }}
+          >
+            Here is a list of possible alternatives. Do any appeal to you?
+          </div>
+          <input style={{ marginBottom: "1.5%" }}></input>
+        </div>
+      </div>
     </div>
   );
 }
