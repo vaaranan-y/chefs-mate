31386bed91f9b1f24625db21a4cd83d052206b91
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-01-22 03:03:48 -0500

    Cleaned up landing page

--- Frontend/chefs-mate-frontend/src/homepage.js
+++ Frontend/chefs-mate-frontend/src/homepage.js
@@ -1,33 +1,35 @@
 import React from "react";
-import Logo from "./Chefs Mate.png"
+import Logo from "./Chefs Mate.png";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
-
-
-function Homepage(){
-
-    const button = {
-        backgroundColor: "#FFDE59",
-        color: "black",
-        padding: "20px 60px",
-        paddingTop: "10px",
-        borderRadius: "5px",
-        cursor: "pointer",
-        borderWidth:"0",
-        margin: "10px 50px"
-    } 
-    return (
-        <div>
-    <div className="homepage" style={{backgroundColor: "#43865E", height: '50%' }}>
-        <img src={Logo} style={{width: 500, height: 500, marginLeft:400}}></img>
+function Homepage() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: "#43865E",
+          height: "50%",
+          alignItems: "center",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img src={Logo}></img>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button
+          onClick={() => {
+            console.log("HELLO");
+            navigate("./login.js");
+          }}
+          variant="outline-warning"
+        >
+          LET'S START
+        </Button>
+      </div>
     </div>
-    <div style={{marginLeft:500}}>
-    <button onClick={event =>  window.location.href='./login.js'} style={button}>
-  Let's start!
-</button>
-</div>
-    </div>
-    );
+  );
 }
 export default Homepage;
-
-
